5dc6ff12a05145b54f15157cd2ff41d29e6f8082
Ion Ambrinoc <iambrinoc@gmail.com>
2015-02-23 12:23:05 +0000

    completed code, fix errors

---
+++ alice_server.py
@@ -0,0 +1,99 @@
+###SAMPLE CODE FOR SERVER PROCESS - "ALICE"############################################################
+
+import socket 				# note that Alice is one of the participants, the one who
+import sys					# does not invert numbers modulo p; she acts as a server
+from thread import *		# for simplicity of implementation only, and this is not
+							# necessary for the purposes of the (abstract) protocol.
+
+from crypto.util import number   # generate random large prime (p in specification)
+prime = number.getPrime(512)	 # (first ingredient for Diffie-Hellman); all
+p = prime
+
+u = number.getRandomInteger(128) # generate u coprime with p; Diffie-Hellman base
+import fractions				 # (second ingredient for Diffie-Hellman)
+while fractions.gcd(u,prime)!=1:
+	u = number.getRandomInteger(128)
+
+from Crypto.Util import number 		# generate fresh random numbers for 
+rA  = number.getRandomInteger(128)	# use in protocol; Bob also does this step
+rAp = number.getRandomInteger(128)
+rApp= number.getRandomInteger(128)
+
+from Crypto.Hash import MD5
+h = MD5.new()
+secret = 12345678					#manually define secret; same to be done for Bob
+h.update(str(secret))				#then hash secret using MD5
+Hs = h.digest()
+print "Hash of secret: ", Hs
+
+urA  = u^rA%p             			#exponentiate for Diffie-Hellman
+urAp = u^rAp%p
+print "Generated ", urA, urAp
+
+#cryptographic component generation now done.
+#from now on act as a socket server and just pass messages on
+
+HOST = ''  			# listen on all connections
+PORT = 4321
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'Socket created'
+
+try:					#Bind socket to local host and port
+    s.bind((HOST, PORT))
+except socket.error , msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+print 'Socket bind complete'
+
+s.listen(10)				#Start listening on socket
+print 'Socket now listening'
+
+#Function for handling connections, to be used to create thread for Bob;
+#very simplified to make not a general socket server, but one that strictly
+#carries out required protocol with Bob.
+def clientthread(conn):
+	conn.sendall(str(prime))	# message 0: pass Bob ingredients for Diffie-Hellman
+	conn.sendall(str(u))		# as strings (sendall does not take longs)
+        
+	conn.sendall(str(urA))		# message 1: two Diffie-Hellman messages.
+	conn.sendall(str(urAp))
+
+	urB = long(conn.recv(128))	# message 2: Bob's corresponding D-H messages.
+	urBp= long(conn.recv(128))  # reconverted into (useful) longs.
+
+	x     = (urB    ^ rA ) % p 	# x=u^(r_ar_b) mod p
+	y     = (urBp   ^ rAp) % p 	# y = u^(r'_ar'_b) mod p
+	xa 	  = (x^Hs)         % p	# x_a=x^H(s_a), known by Alice only
+	yrppa = (y^rApp)	   % p  # y^r''_a for fresh r''_a generated above 
+
+	conn.sendall(str(yrppa))    # message 3
+	yrppb = long(conn.recv(128))# message 4, from Bob: y^(-r''_b)
+
+	z = yrppb^rApp			  % p   # known by both, to be used later in the check.
+	aliceCheck = xa*(u^(rAp)) % p
+
+	conn.sendall(str(aliceCheck))   # message 5, relying  on hash and discrete 
+									# to ensure secret is not leaked; this is the
+									# only place where a function of the secret is
+									# actually transferred between Alice and Bob.
+	bobCheck = long(conn.recv(128)) # message 6, Bob's analogous to 5.
+
+	t = aliceCheck * bobCheck % p
+
+	rBp = long(conn.recv(128))		# reverse message order; Bob discloses one of the 
+	if (u^rBp) % p != urBp:
+		print "BOB IS LYING, DROPPING CONNECTION"
+		s.close()					#and drop connection; otherwise...
+	conn.sendall(str(rAp))			# message 8; do the same as Bob
+
+	if (t^(rAp * rBp)) % p == z:
+		print "The secrets match!"
+	else: print "The secrets don't match."
+
+    conn.close()				#come out of loop
+ 
+while 1:						#now talk to Bob
+    conn, addr = s.accept()		#wait to accept connection; blocking call
+    print 'Connected with ' + addr[0] + ':' + str(addr[1]) + 'as Bob'
+    start_new_thread(clientthread ,(conn,))
+s.close()
--- bob.py
+++ bob.py
@@ -3,22 +3,24 @@
 import socket   #for sockets
 import sys  #for exit
  
-# generate fresh random numbers for use in protocol:
-from Crypto.Util import number
-rB  = number.getRandomInteger(128)
+from Crypto.Util import number      # generate fresh random numbers for 
+rB  = number.getRandomInteger(128)  # use in protocol; 
 rBp = number.getRandomInteger(128)
 rBpp= number.getRandomInteger(128)
 
 from Crypto.Hash import MD5
 h = MD5.new()
-secret = 12345678
-h.update(str(secret))
+secret = 12345678                   #manually define secret; same to be done for Bob
+h.update(str(secret))               #then hash secret using MD5
 Hs = h.digest()
-print Hs
+print "Hash of secret: ", Hs
+
+urB  = u^rB%p                       #exponentiate for Diffie-Hellman
+urBp = u^rBp%p
+print "Generated ", urB, urBp
 
 #PKAlice =
 #SK =
-
 
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -32,36 +34,67 @@ port = 4323;
 try:
     remote_ip = socket.gethostbyname( host ) 
 except socket.gaierror:
-    #could not resolve
     print 'Hostname could not be resolved. Exiting'
     sys.exit()
  
 s.connect((remote_ip , port))				#Connect to remote server
 print 'Socket Connected to ' + host + ' on ip ' + remote_ip
  
-#Now receive data
 p = long(s.recv(512))	#receive Diffie-Hellman ingredients from server
 u = long(s.recv(128))	#message 0 in protocol spec.
 print("Received DH ingredients")
 
-#Send identification to server
-message = "BOB" 
-try :
-    #Set the whole string
-    s.sendall(message)
-except socket.error:
-    #Send failed
-    print 'Send failed'
-    sys.exit()
-print 'Identification sent successfully'
+urA  = long(s.recv(128))    #receive message 1
+urAp = long(s.recv(128))
 
 urB  = u^rB%p
 urBp = u^rBp%p
 print "Generated ", urB, urBp
 
-urA  = long(s.recv(128)) 	#receive message 1
-urAp = long(s.recv(128))
-s.sendall(str(urB))		#send message 2
-s.sendall(str(urBp))	
+try :                       #send message 2
+    s.sendall(str(urB))
+except socket.error:
+    print 'Send failed'
+    sys.exit()
+try :                       
+    s.sendall(str(urBp))
+except socket.error:
+    print 'Send failed'
+    sys.exit()
 
+x     = (urA    ^ rB ) % p  # x=u^(r_ar_b) mod p
+y     = (urAp   ^ rBp) % p  # y = u^(r'_ar'_b) mod p
+xb    = (x^Hs)         % p  # x_b=H^H(s_b) known only by Bob
+yrppb = (y^rBpp)       % p  # y^r''_b for fresh r''_b generated above
+
+yrppa = long(s.recv(128))   # receive message 3
+try :                       #send message 4
+    s.sendall(str(yrppb))
+except socket.error:
+    print 'Send failed'
+    sys.exit()
+
+z = yrppa^rBpp      #known by both, to be used later in the check
+bobCheck = xb ^ (u^(rBp))
+
+aliceCheck = long(s.recv(128)) # message 5
+try :                       #send message 6
+    s.sendall(str(bobCheck))
+except socket.error:
+    print 'Send failed'
+    sys.exit()
+
+t = aliceCheck * bobCheck % p
+try :                       #send message 7; disclose random
+    s.sendall(str(rBp))
+except socket.error:
+    print 'Send failed'
+    sys.exit()
+
+rAp = long(s.recv(128))     #message 8
+
+if (t^(rAp * rBp)) % p == z:
+          print "The secrets match!"
+    else: print "The secrets don't match."
+s.close() #done
 print urA, urAp, urB, urBp
